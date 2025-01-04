562521e316d3ce74932cc798afb069ca90ccf175
syntet1c <gabrielstojak@proton.me>
2025-01-04 03:00:01 +0100

    Python portscanner
    currently in dev

---
+++ portscanner.py
@@ -0,0 +1,28 @@
+import socket
+import multiprocessing
+import argparse
+
+def arguments():
+    args = argparse.ArgumentParser()
+    args.add_argument("-t", "--target", dest="target", help="Your target, eg. example.com")
+    args.add_argument("-p", "--ports", dest="port", help="port range, eg. -p 1-1024")
+    argument = args.parse_args()
+    port = str(argument.port).split("-")
+    port = range(int(port[0]), int(port[1]))
+    
+    return [argument.target, port]
+
+h = socket.gethostbyname(arguments()[0])
+def scann_port(port):
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(1)
+    
+    
+    r = s.connect_ex((h, port))
+    if r == 0:
+        print(f"Port Open: {port}")
+    s.close()
+
+with multiprocessing.Pool(processes=70) as p:
+    p.map(scann_port, arguments()[1])
